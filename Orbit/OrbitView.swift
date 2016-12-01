8b05826279c677e47bb421f0a9c077f4485f6a4c
viwii <lijie121210@163.com>
2016-12-01 23:07:58 +0800

    no change, convert to swift 3.0

--- Orbit/OrbitView.swift
+++ Orbit/OrbitView.swift
@@ -11,71 +11,71 @@ import UIKit
 
 class OrbitView: UIView {
     
-    private var displayLink: CADisplayLink? // 定时器
-    private var starView: UIView? // 主行星
-    private var satelliteAB: UIView?
-    private var satelliteCD: UIView?
-    private var axisLineAB: Array<CGPoint>?
-    private var axisLineCD: Array<CGPoint>?
+    fileprivate var displayLink: CADisplayLink? // 定时器
+    fileprivate var starView: UIView? // 主行星
+    fileprivate var satelliteAB: UIView?
+    fileprivate var satelliteCD: UIView?
+    fileprivate var axisLineAB: Array<CGPoint>?
+    fileprivate var axisLineCD: Array<CGPoint>?
     
-    private weak var starLayer: CAShapeLayer?
-    private weak var sateABLayer: CAShapeLayer?
-    private weak var sateCDLayer: CAShapeLayer?
+    fileprivate weak var starLayer: CAShapeLayer?
+    fileprivate weak var sateABLayer: CAShapeLayer?
+    fileprivate weak var sateCDLayer: CAShapeLayer?
     
-    private var angle = 0
+    fileprivate var angle = 0
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.initilize()
     }
-    override func encodeWithCoder(aCoder: NSCoder) {
-        super.encodeWithCoder(aCoder)
+    override func encode(with aCoder: NSCoder) {
+        super.encode(with: aCoder)
         if let link = self.displayLink {
-            aCoder.encodeObject(link, forKey: "displayLink")
+            aCoder.encode(link, forKey: "displayLink")
         }
         if let star = self.starView {
-            aCoder.encodeObject(star, forKey: "starView")
+            aCoder.encode(star, forKey: "starView")
         }
         if let ab = self.satelliteAB {
-            aCoder.encodeObject(ab, forKey: "satelliteAB")
+            aCoder.encode(ab, forKey: "satelliteAB")
         }
         if let cd = self.satelliteCD {
-            aCoder.encodeObject(cd, forKey: "satelliteCD")
+            aCoder.encode(cd, forKey: "satelliteCD")
         }
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.displayLink = aDecoder.decodeObjectForKey("displayLink") as? CADisplayLink
-        self.starView = aDecoder.decodeObjectForKey("starView") as? UIView
-        self.satelliteAB = aDecoder.decodeObjectForKey("satelliteAB") as? UIView
-        self.satelliteCD = aDecoder.decodeObjectForKey("satelliteCD") as? UIView
-        self.axisLineAB = aDecoder.decodeObjectForKey("axisLineAB") as? Array<CGPoint>
-        self.axisLineCD = aDecoder.decodeObjectForKey("axisLineCD") as? Array<CGPoint>
+        self.displayLink = aDecoder.decodeObject(forKey: "displayLink") as? CADisplayLink
+        self.starView = aDecoder.decodeObject(forKey: "starView") as? UIView
+        self.satelliteAB = aDecoder.decodeObject(forKey: "satelliteAB") as? UIView
+        self.satelliteCD = aDecoder.decodeObject(forKey: "satelliteCD") as? UIView
+        self.axisLineAB = aDecoder.decodeObject(forKey: "axisLineAB") as? Array<CGPoint>
+        self.axisLineCD = aDecoder.decodeObject(forKey: "axisLineCD") as? Array<CGPoint>
         self.initilize()
     }
-    private func initilize() {
+    fileprivate func initilize() {
         let f = self.frame
         let o = self.center
         let b = self.bounds
 
         assert(b.width >= 120 && b.height >= 120, "frame too small...")
         
-        self.axisLineAB = [CGPoint(x: CGRectGetMinX(f), y: o.y),CGPoint(x: CGRectGetMaxX(f), y: o.y)]
-        self.axisLineCD = [CGPoint(x: o.x, y: CGRectGetMinY(f)),CGPoint(x: o.x, y: CGRectGetMaxY(f))]
+        self.axisLineAB = [CGPoint(x: f.minX, y: o.y),CGPoint(x: f.maxX, y: o.y)]
+        self.axisLineCD = [CGPoint(x: o.x, y: f.minY),CGPoint(x: o.x, y: f.maxY)]
         
-        self.backgroundColor = UIColor.lightGrayColor()
+        self.backgroundColor = UIColor.lightGray
         
         if self.starView == nil {
             self.starLayer = self.circleShapeLayer(60.0)
             self.starView = UIView(frame: CGRect(x: b.width / 2.0 - 60, y: b.height / 2.0 - 60, width: 120, height: 120))
-            self.starView?.backgroundColor = UIColor.clearColor()
+            self.starView?.backgroundColor = UIColor.clear
             self.starView?.layer.addSublayer(self.starLayer!)
             self.addSubview(starView!)
         }
         if self.satelliteAB == nil {
             self.sateABLayer = self.circleShapeLayer(90.0)
             self.satelliteAB = UIView(frame: CGRect(x: b.width / 2.0 - 90, y: b.height / 2.0 - 90, width: 180, height: 180))
-            self.satelliteAB?.backgroundColor = UIColor.clearColor()
+            self.satelliteAB?.backgroundColor = UIColor.clear
             self.satelliteAB?.layer.addSublayer(self.sateABLayer!)
             self.addSubview(satelliteAB!)
         }
@@ -92,20 +92,20 @@ class OrbitView: UIView {
     
     //
     
-    private func startDisplayLink() {
+    fileprivate func startDisplayLink() {
         if let _ = self.displayLink {
             self.stopDisplayLink()
         }
         self.displayLink = CADisplayLink(target: self, selector: #selector(OrbitView.fireDisplayLink))
         self.displayLink?.frameInterval = 5
-        self.displayLink?.addToRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+        self.displayLink?.add(to: RunLoop.current, forMode: RunLoopMode.defaultRunLoopMode)
     }
-    private func pauseDisplayLink() {
+    fileprivate func pauseDisplayLink() {
         if let link = self.displayLink {
-            link.paused = true
+            link.isPaused = true
         }
     }
-    private func stopDisplayLink() {
+    fileprivate func stopDisplayLink() {
         if let link = self.displayLink {
             link.invalidate()
             self.displayLink = nil
@@ -123,13 +123,13 @@ class OrbitView: UIView {
     
     //
     
-    private func circleShapeLayer(radius: CGFloat) -> CAShapeLayer {
+    fileprivate func circleShapeLayer(_ radius: CGFloat) -> CAShapeLayer {
         let cpoint = CGPoint(x: radius, y: radius)
         let cpath = UIBezierPath(arcCenter: cpoint, radius: radius, startAngle: 0, endAngle: 2 * CGFloat(M_PI), clockwise: true)
         let circle = CAShapeLayer()
-        circle.path = cpath.CGPath
-        circle.fillColor = UIColor.clearColor().CGColor
-        circle.strokeColor = UIColor.darkGrayColor().CGColor
+        circle.path = cpath.cgPath
+        circle.fillColor = UIColor.clear.cgColor
+        circle.strokeColor = UIColor.darkGray.cgColor
         circle.lineWidth = 1.0
         circle.strokeStart = 0
         circle.strokeEnd = 2 * CGFloat(M_PI)
@@ -138,14 +138,14 @@ class OrbitView: UIView {
     
     //
     
-    private func animateStarView() {
+    fileprivate func animateStarView() {
         let endanimate = CABasicAnimation(keyPath: "strokeEnd")
         endanimate.fromValue = 0
         endanimate.toValue = 1
         endanimate.duration = 4
         endanimate.repeatCount = MAXFLOAT
         endanimate.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
-        self.starLayer?.addAnimation(endanimate, forKey: "star_strokeEnd")
+        self.starLayer?.add(endanimate, forKey: "star_strokeEnd")
         
         let startanimate = CABasicAnimation(keyPath: "strokeStart")
         startanimate.fromValue = 0
@@ -153,17 +153,17 @@ class OrbitView: UIView {
         startanimate.duration = 4
         startanimate.repeatCount = MAXFLOAT
         startanimate.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
-        self.starLayer?.addAnimation(startanimate, forKey: "star_strokeStart")
+        self.starLayer?.add(startanimate, forKey: "star_strokeStart")
     }
     
-    private func animateSatelliteAB() {
+    fileprivate func animateSatelliteAB() {
         let rotationX = CABasicAnimation(keyPath: "transform.rotation.x")
         rotationX.fromValue = -CGFloat(M_PI_2)
         rotationX.toValue = CGFloat(M_PI_2)
         rotationX.repeatCount = MAXFLOAT
         rotationX.duration = 5
         rotationX.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-        self.satelliteAB?.layer.addAnimation(rotationX, forKey: "sateAB_x")
+        self.satelliteAB?.layer.add(rotationX, forKey: "sateAB_x")
         
         let animateZ = CABasicAnimation(keyPath: "transform.rotation.z")
         animateZ.fromValue = -CGFloat(M_PI) * 30.0 / 180
@@ -171,7 +171,7 @@ class OrbitView: UIView {
         animateZ.repeatCount = MAXFLOAT
         animateZ.duration = 5
         animateZ.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        self.satelliteAB?.layer.addAnimation(animateZ, forKey: "sateAB_z")
+        self.satelliteAB?.layer.add(animateZ, forKey: "sateAB_z")
         
         let animateY = CABasicAnimation(keyPath: "transform.rotation.y")
         animateY.fromValue = -CGFloat(M_PI) * 30.0 / 180
@@ -179,10 +179,10 @@ class OrbitView: UIView {
         animateY.repeatCount = MAXFLOAT
         animateY.duration = 5
         animateY.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        self.satelliteAB?.layer.addAnimation(animateY, forKey: "sateAB_y")
+        self.satelliteAB?.layer.add(animateY, forKey: "sateAB_y")
     }
     
-    private func animateSatelliteCD() {
+    fileprivate func animateSatelliteCD() {
         let rotationY = CABasicAnimation(keyPath: "transform.rotation.y")
         rotationY.fromValue = CGFloat(M_PI_2) * 90.0 / 180
         rotationY.toValue = CGFloat(M_PI_2) * 120.0 / 180
@@ -190,7 +190,7 @@ class OrbitView: UIView {
         rotationY.autoreverses = true
         rotationY.duration = 7
         rotationY.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        self.satelliteCD?.layer.addAnimation(rotationY, forKey: "sateCD_y")
+        self.satelliteCD?.layer.add(rotationY, forKey: "sateCD_y")
         
         let rotationZ = CABasicAnimation(keyPath: "transform.rotation.z")
         rotationZ.fromValue = -CGFloat(M_PI) * 20.0 / 180
@@ -199,7 +199,7 @@ class OrbitView: UIView {
         rotationZ.autoreverses = true
         rotationZ.duration = 7
         rotationZ.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        self.satelliteCD?.layer.addAnimation(rotationZ, forKey: "sateCD_z")
+        self.satelliteCD?.layer.add(rotationZ, forKey: "sateCD_z")
         
         let rotationX = CABasicAnimation(keyPath: "transform.rotation.x")
         rotationX.fromValue = -CGFloat(M_PI) * 20.0 / 180
@@ -208,6 +208,6 @@ class OrbitView: UIView {
         rotationX.autoreverses = true
         rotationX.duration = 7
         rotationX.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-        self.satelliteCD?.layer.addAnimation(rotationX, forKey: "sateCD_x")
+        self.satelliteCD?.layer.add(rotationX, forKey: "sateCD_x")
     }
 }
