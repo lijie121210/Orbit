abc6da785e8b9a2e66f8ddd289e3f0f0166191c6
viwii <lijie121210@163.com>
2017-03-03 10:46:16 +0800

    add image

--- Orbit/OrbitViewController.swift
+++ Orbit/OrbitViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class OrbitViewController: UIViewController {
     
-    
     func test2() {
         let vi = OrbitSphere(frame: CGRect(x: 0, y: 100, width: view.bounds.width, height: view.bounds.width))
         
@@ -39,30 +38,8 @@ class OrbitViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
         
         test1()
-        
-        
-        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Orbit
 Try to imp this animation : https://dribbble.com/shots/2943004-Orbit
+
+Slag quality ah：
+![] (/img/oribitsample2.gif?raw=true "oribit sample")
